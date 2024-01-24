f65ff2262ab4e53cce7abccf3d374fde729dd9a8
Zan Makarov <z.makarov@codat.io>
2024-01-24 19:19:47 +0000

    sidebar

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,45 @@
 import "../../../node_modules/@codat/orchard-ui/dist/index.css";
 import "../../index.css";
 
-import { OrchardProvider } from "@codat/orchard-ui";
+import { OrchardProvider, Sidebar } from "@codat/orchard-ui";
 import * as React from "react";
 import { BrowserRouter } from "react-router-dom";
 
 import styles from "./App.module.scss";
 import AppRouter from "./AppRouter";
 
-const App: React.FC = () => (
-  <div className={styles.app}>
-    <OrchardProvider appId="template-app">
-      <BrowserRouter>
-        <AppRouter />
-      </BrowserRouter>
-    </OrchardProvider>
-  </div>
-);
+const App: React.FC = () => {
+  const sidebarGroups = [
+    {
+      groupName: "Data types",
+      items: [
+        {
+          name: "Supplier",
+          route: {
+            pathname: "/supplier",
+          },
+        },
+        {
+          name: "Bill",
+          route: {
+            pathname: "/bill",
+          },
+        },
+      ],
+    },
+  ];
+  return (
+    <div className={styles.app}>
+      <OrchardProvider appId="template-app">
+        <BrowserRouter>
+          <div className={styles.nav}>
+            <Sidebar sidebarGroups={sidebarGroups} />
+            <AppRouter />
+          </div>
+        </BrowserRouter>
+      </OrchardProvider>
+    </div>
+  );
+};
 
 export default App;
